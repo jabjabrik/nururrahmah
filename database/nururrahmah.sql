a1dd619aae83f814f2bc447304a34cfe1d64fff4
ucupss <yusufjabriko@gmail.com>
2025-08-08 10:19:56 +0700

    update db

--- database/nururrahmah.sql
+++ database/nururrahmah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 27 Jul 2025 pada 18.42
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 7.3.33
+-- Host: localhost:3306
+-- Generation Time: Aug 08, 2025 at 03:19 AM
+-- Server version: 5.7.39
+-- PHP Version: 7.3.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `galeri`
+-- Table structure for table `galeri`
 --
 
 CREATE TABLE `galeri` (
@@ -34,7 +34,7 @@ CREATE TABLE `galeri` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `galeri`
+-- Dumping data for table `galeri`
 --
 
 INSERT INTO `galeri` (`id_galeri`, `foto`, `deskripsi`) VALUES
@@ -50,7 +50,7 @@ INSERT INTO `galeri` (`id_galeri`, `foto`, `deskripsi`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `informasi`
+-- Table structure for table `informasi`
 --
 
 CREATE TABLE `informasi` (
@@ -63,20 +63,21 @@ CREATE TABLE `informasi` (
   `visi` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `misi` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ppdb` enum('1','0') COLLATE utf8mb4_unicode_ci NOT NULL
+  `ppdb` enum('1','0') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pengumuman` enum('1','0') COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `informasi`
+-- Dumping data for table `informasi`
 --
 
-INSERT INTO `informasi` (`id_informasi`, `email`, `no_telepon`, `jumlah_siswa_ra`, `jumlah_siswa_mi`, `jumlah_guru`, `visi`, `misi`, `alamat`, `ppdb`) VALUES
-(1, 'nururrahmah@gmail.com', '6281259103634', '20', '35', '12', 'Mewujudkan generasi yang unggul dalam bidang A.P.I.K (Afektif, Psikomotorik, Ilmu Pengetahuan dan Teknologi, serta Keagamaan), berbudi pekerti luhur, berdaya saing, serta mampu beradaptasi dengan perkembangan global tanpa meninggalkan nilai-nilai keislaman sebagai landasan kehidupan.', 'Menyelenggarakan pembinaan akhlak mulia sejak usia dini hingga jenjang madrasah. \nMemberikan pendidikan dasar yang berorientasi pada keimanan, keislaman, dan kecintaan terhadap ilmu agama. \nMengembangkan keterampilan siswa sesuai dengan potensi dan kompetensinya di setiap jenjang pendidikan. \nMembimbing siswa dalam memahami dasar-dasar ilmu pengetahuan yang sesuai dengan tahap perkembangan mereka.', 'MI NURUR RAHMAH merupakan salah satu sekolah jenjang MI berstatus Swasta yang berada di wilayah Desa Tigasan Wetan, Kec. Leces, Kab. Probolinggo, Jawa Timur. MIS NURUR RAHMAH didirikan pada tanggal 3 Juli 1993 dengan Nomor SK Pendirian Mm.18/00.00/PP.03.2/00932/1993 yang berada dalam naungan Kementerian Agama. Operator yang bertanggung jawab adalah Abdul Rasyid Rasyidi.', '1');
+INSERT INTO `informasi` (`id_informasi`, `email`, `no_telepon`, `jumlah_siswa_ra`, `jumlah_siswa_mi`, `jumlah_guru`, `visi`, `misi`, `alamat`, `ppdb`, `pengumuman`) VALUES
+(1, 'nururrahmah@gmail.com', '6281259103634', '20', '35', '12', 'Mewujudkan generasi yang unggul dalam bidang A.P.I.K (Afektif, Psikomotorik, Ilmu Pengetahuan dan Teknologi, serta Keagamaan), berbudi pekerti luhur, berdaya saing, serta mampu beradaptasi dengan perkembangan global tanpa meninggalkan nilai-nilai keislaman sebagai landasan kehidupan.', 'Menyelenggarakan pembinaan akhlak mulia sejak usia dini hingga jenjang madrasah. \nMemberikan pendidikan dasar yang berorientasi pada keimanan, keislaman, dan kecintaan terhadap ilmu agama. \nMengembangkan keterampilan siswa sesuai dengan potensi dan kompetensinya di setiap jenjang pendidikan. \nMembimbing siswa dalam memahami dasar-dasar ilmu pengetahuan yang sesuai dengan tahap perkembangan mereka.', 'MI NURUR RAHMAH merupakan salah satu sekolah jenjang MI berstatus Swasta yang berada di wilayah Desa Tigasan Wetan, Kec. Leces, Kab. Probolinggo, Jawa Timur. MIS NURUR RAHMAH didirikan pada tanggal 3 Juli 1993 dengan Nomor SK Pendirian Mm.18/00.00/PP.03.2/00932/1993 yang berada dalam naungan Kementerian Agama. Operator yang bertanggung jawab adalah Abdul Rasyid Rasyidi.', '1', '0');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `siswa`
+-- Table structure for table `siswa`
 --
 
 CREATE TABLE `siswa` (
@@ -102,36 +103,37 @@ CREATE TABLE `siswa` (
   `pekerjaan_ayah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pekerjaan_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pendidikan_ortu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `penghasilan_ayah` int(11) NOT NULL DEFAULT 0,
-  `penghasilan_ibu` int(11) NOT NULL DEFAULT 0,
+  `penghasilan_ayah` int(11) NOT NULL DEFAULT '0',
+  `penghasilan_ibu` int(11) NOT NULL DEFAULT '0',
   `nama_ayah_wali` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nama_ibu_wali` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pekerjaan_wali` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pendidikan_terakhir` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `penghasilan_wali` int(11) NOT NULL DEFAULT 0,
+  `penghasilan_wali` int(11) NOT NULL DEFAULT '0',
   `foto_3x4` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `akta_kelahiran` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `kartu_keluarga` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ktp_ortu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tahun_ajaran` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status_diterima` enum('diterima','tidak diterima') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `tanggal_diterima` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `siswa`
+-- Dumping data for table `siswa`
 --
 
-INSERT INTO `siswa` (`id_siswa`, `nama_lengkap`, `nama_panggilan`, `nomor_induk_asal`, `nisn`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `anak_ke`, `status_anak`, `alamat`, `no_hp`, `nama_ra_tk_asal`, `alamat_ra_tk_asal`, `nama_sd_mi_asal`, `alamat_sd_mi_asal`, `nama_ayah`, `nama_ibu`, `pekerjaan_ayah`, `pekerjaan_ibu`, `pendidikan_ortu`, `penghasilan_ayah`, `penghasilan_ibu`, `nama_ayah_wali`, `nama_ibu_wali`, `pekerjaan_wali`, `pendidikan_terakhir`, `penghasilan_wali`, `foto_3x4`, `akta_kelahiran`, `kartu_keluarga`, `ktp_ortu`, `status_diterima`, `tanggal_diterima`) VALUES
-(1, 'Ahmad Fauzi', 'Ahmad', 'NIS-2023-001', '0031234567', 'Jakarta', '2015-05-10', 'Laki-laki', 'Islam', '1', 'Kandung', 'Jl. Merdeka No. 123, Jakarta Pusat', '081234567890', 'TK Bunga Bangsa', 'Jl. Melati No. 5, Jakarta', 'SDN 01 Jakarta', 'Jl. Pendidikan No. 10, Jakarta', 'Budi Santoso', 'Siti Aminah', 'Pegawai Negeri', 'Guru', 'S1', 4000000, 3000000, '', '', '', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', 'diterima', '2023-06-15'),
-(2, 'Siti Nurhaliza', 'Siti', 'NIS-2023-002', '0032345678', 'Bandung', '2015-08-20', 'Perempuan', 'Islam', '2', 'Kandung', 'Jl. Cendana No. 45, Bandung', '082345678901', 'TK Pelangi', 'Jl. Pelangi No. 12, Bandung', '', '', 'Joko Widodo', 'Ani Yudhoyono', 'Wiraswasta', 'Ibu Rumah Tangga', 'SMA', 3000000, 0, '', '', '0', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', 'diterima', '2023-06-15'),
-(3, 'Budi Setiawan', 'Budi', 'NIS-2023-003', '0033456789', 'Surabaya', '2014-11-15', 'Laki-laki', 'Islam', '3', 'Kandung', 'Jl. Pahlawan No. 67, Surabaya', '083456789012', 'TK Harapan Bangsa', 'Jl. Harapan No. 8, Surabaya', '', '', 'Agus Suparman', 'Dewi Lestari', 'Karyawan Swasta', 'Karyawan Swasta', 'D3', 3000000, 2000000, '', '', '', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', 'tidak diterima', NULL),
-(4, 'Dewi Sartika', 'Dewi', 'NIS-2023-004', '0034567890', 'Bogor', '2015-02-28', 'Perempuan', 'Islam', '1', 'Kandung', 'Jl. Kencana No. 89, Bogor', '084567890123', 'TK Mutiara Hati', 'Jl. Mutiara No. 3, Bogor', '', '', 'Eko Prasetyo', 'Ratna Dewi', 'Dokter', 'Perawat', 'S2', 5000000, 2000000, '', '', '', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', 'diterima', '2023-06-15'),
-(5, 'Eko Prasetyo', 'Eko', 'NIS-2023-005', '0035678901', 'Depok', '2014-09-05', 'Laki-laki', 'Islam', '2', 'Kandung', 'Jl. Anggrek No. 34, Depok', '085678901234', 'TK Bintang Kecil', 'Jl. Bintang No. 7, Depok', '', '', 'Fajar Nugroho', 'Gita Maya', 'Supir', 'Penjahit', 'SMP', 2000000, 1000000, 'Hadi Susanto', 'Indah Permata', 'Pedagang', 'SMA', 2000000, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', NULL, NULL);
+INSERT INTO `siswa` (`id_siswa`, `nama_lengkap`, `nama_panggilan`, `nomor_induk_asal`, `nisn`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `anak_ke`, `status_anak`, `alamat`, `no_hp`, `nama_ra_tk_asal`, `alamat_ra_tk_asal`, `nama_sd_mi_asal`, `alamat_sd_mi_asal`, `nama_ayah`, `nama_ibu`, `pekerjaan_ayah`, `pekerjaan_ibu`, `pendidikan_ortu`, `penghasilan_ayah`, `penghasilan_ibu`, `nama_ayah_wali`, `nama_ibu_wali`, `pekerjaan_wali`, `pendidikan_terakhir`, `penghasilan_wali`, `foto_3x4`, `akta_kelahiran`, `kartu_keluarga`, `ktp_ortu`, `tahun_ajaran`, `status_diterima`, `tanggal_diterima`) VALUES
+(1, 'Ahmad Fauzi', 'Ahmad', 'NIS-2023-001', '0031234567', 'Jakarta', '2015-05-10', 'Laki-laki', 'Islam', '1', 'Kandung', 'Jl. Merdeka No. 123, Jakarta Pusat', '081234567890', 'TK Bunga Bangsa', 'Jl. Melati No. 5, Jakarta', 'SDN 01 Jakarta', 'Jl. Pendidikan No. 10, Jakarta', 'Budi Santoso', 'Siti Aminah', 'Pegawai Negeri', 'Guru', 'S1', 4000000, 3000000, '', '', '', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', '2025/2026', 'diterima', '2023-06-15'),
+(2, 'Siti Nurhaliza', 'Siti', 'NIS-2023-002', '0032345678', 'Bandung', '2015-08-20', 'Perempuan', 'Islam', '2', 'Kandung', 'Jl. Cendana No. 45, Bandung', '082345678901', 'TK Pelangi', 'Jl. Pelangi No. 12, Bandung', '', '', 'Joko Widodo', 'Ani Yudhoyono', 'Wiraswasta', 'Ibu Rumah Tangga', 'SMA', 3000000, 0, '', '', '0', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', '2025/2026', 'diterima', '2023-06-15'),
+(3, 'Budi Setiawan', 'Budi', 'NIS-2023-003', '0033456789', 'Surabaya', '2014-11-15', 'Laki-laki', 'Islam', '3', 'Kandung', 'Jl. Pahlawan No. 67, Surabaya', '083456789012', 'TK Harapan Bangsa', 'Jl. Harapan No. 8, Surabaya', '', '', 'Agus Suparman', 'Dewi Lestari', 'Karyawan Swasta', 'Karyawan Swasta', 'D3', 3000000, 2000000, '', '', '', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', '2025/2026', 'tidak diterima', NULL),
+(4, 'Dewi Sartika', 'Dewi', 'NIS-2023-004', '0034567890', 'Bogor', '2015-02-28', 'Perempuan', 'Islam', '1', 'Kandung', 'Jl. Kencana No. 89, Bogor', '084567890123', 'TK Mutiara Hati', 'Jl. Mutiara No. 3, Bogor', '', '', 'Eko Prasetyo', 'Ratna Dewi', 'Dokter', 'Perawat', 'S2', 5000000, 2000000, '', '', '', '', 0, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', '2025/2026', 'diterima', '2023-06-15'),
+(5, 'Eko Prasetyo', 'Eko', 'NIS-2023-005', '0035678901', 'Depok', '2014-09-05', 'Laki-laki', 'Islam', '2', 'Kandung', 'Jl. Anggrek No. 34, Depok', '085678901234', 'TK Bintang Kecil', 'Jl. Bintang No. 7, Depok', '', '', 'Fajar Nugroho', 'Gita Maya', 'Supir', 'Penjahit', 'SMP', 2000000, 1000000, 'Hadi Susanto', 'Indah Permata', 'Pedagang', 'SMA', 2000000, 'foto_3x4.jpg', 'akta.jpg', 'kk.jpg', 'ktp.jpg', '2025/2026', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -142,65 +144,65 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `nama_user`, `username`, `password`) VALUES
-(1, 'administrator', 'admin', '$2y$12$A8MMBP.biu.NxzMauJNIsuIhkiKv/3qHO1QgUh9.9K6.4lFBUZcQe');
+(1, 'administrator', 'admin', '$2y$12$iS0FMnddhRduZ7TBfbteGeYQP.rQMN333jYXlkdzwJFXjLikjoTma');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `galeri`
+-- Indexes for table `galeri`
 --
 ALTER TABLE `galeri`
   ADD PRIMARY KEY (`id_galeri`);
 
 --
--- Indeks untuk tabel `informasi`
+-- Indexes for table `informasi`
 --
 ALTER TABLE `informasi`
   ADD PRIMARY KEY (`id_informasi`);
 
 --
--- Indeks untuk tabel `siswa`
+-- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD PRIMARY KEY (`id_siswa`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `user_username_unique` (`username`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `galeri`
+-- AUTO_INCREMENT for table `galeri`
 --
 ALTER TABLE `galeri`
   MODIFY `id_galeri` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT untuk tabel `informasi`
+-- AUTO_INCREMENT for table `informasi`
 --
 ALTER TABLE `informasi`
   MODIFY `id_informasi` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `siswa`
+-- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
   MODIFY `id_siswa` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
